001513817754228ad8e65b765049e57f3dae7eaa
KimHyunse <anzmf12@naver.com>
2018-10-15 23:17:38 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './views/Main';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
